8b6145985868b92a0ebda2c94a3a144819b92f10
RomanAlimov <roma.alimov.02@bk.ru>
2025-02-20 23:53:18 +0500

    рп

--- main.go
+++ main.go
@@ -1,28 +1,15 @@
 package main
 
-import ( // необходимые пакеты
+import (
 	"fmt"
-	"log"
 	"net/http"
 )
 
-func logging(next http.Handler) http.Handler { // создал функцию обработчика и вернул ее новой функцией обработчика
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) { //возврат новой функции в ней же запрос и завершение
-		log.Println("Запрос:", r.Method, r.URL.Path) // откладка в результате которого выводится ссылка куда зашел пользователь
-		next.ServeHTTP(w, r)                         // передача инфы
-		log.Println("Завершение отправки запроса.")  // откладка в терминал
-	})
-}
-
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Обработано") //напечатал
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello")
 }
 
 func main() {
-	mux := http.NewServeMux()              //переменная
-	mux.HandleFunc("/hello", helloHandler) // адрес в локалке
-
-	wrappedMux := logging(mux) // передача переменной
-
-	http.ListenAndServe(":8080", wrappedMux) // наш сервер на локалке
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":8080", nil)
 }
